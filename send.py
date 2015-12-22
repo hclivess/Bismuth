bd9e672e587cd1b8586b4fd41cab5675d7d73590
hclivess <translace@gmail.com>
2015-12-22 12:35:53 +0100

    major update of client
    reorganized to send transactions to every known peer (and synchronize
    with them), improved error handling

--- send.py
+++ send.py
@@ -51,151 +51,147 @@ print "Your public key:\n "+ str(public_key_readable)
 # import keys
 
 
-#network client program
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #open peerlist and connect
 with open ("peers.txt", "r") as peer_list:
     peers=peer_list.read()
     peer_tuples = re.findall ("'([\d\.]+)', '([\d]+)'",peers)
     print peer_tuples
 
-connected = 0
-while connected == 0:
-    for tuple in peer_tuples:
-        HOST = tuple[0]
-        #print HOST
-        PORT = int(tuple[1])
-        #print PORT
+for tuple in peer_tuples:
+    HOST = tuple[0]
+    #print HOST
+    PORT = int(tuple[1])
+    #print PORT
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+        print "Connected to "+str(HOST)+" "+str(PORT)
+        #network client program
+
+        s.sendall('Hello, server')
+
+        peer = s.getpeername()
+        data = s.recv(1024) #receive data
+        print 'Received data from '+ str(peer) +"\n"+ str(data)
+
+        #get remote peers into tuples
+        server_peer_tuples = re.findall ("'([\d\.]+)', '([\d]+)'",data)
+        print server_peer_tuples
+        print len(server_peer_tuples)
+        #get remote peers into tuples
+
+        #get local peers into tuples
+        peer_file = open("peers.txt", 'r')
+        peer_tuples = []
+        for line in peer_file:
+            extension = re.findall ("'([\d\.]+)', '([\d]+)'",line)
+            peer_tuples.extend(extension)
+        peer_file.close()
+        print peer_tuples
+        #get local peers into tuples
+
+        for x in server_peer_tuples:
+            if x not in peer_tuples:
+                print str(x)+" is a new peer, saving."
+
+                peer_list_file = open("peers.txt", 'a')
+                peer_list_file.write(x+"\n")
+                peer_list_file.close()        
+                
+            else:
+                print str(x)+" is not a new peer, skipping."
+
+
+                
+        #broadcast
 
         try:
-            s.connect((HOST, PORT))
-            connected = 1
-            print "Connected to "+str(HOST)+" "+str(PORT)
-            break
-        except:
-            print "Cannot connect to "+str(HOST)+" "+str(PORT)
-            pass
-
-#open peerlist and connect
-
-
-s.sendall('Hello, server')
-
-peer = s.getpeername()
-data = s.recv(1024) #receive data
-print 'Received data from '+ str(peer) +"\n"+ str(data)
-
-#get remote peers into tuples
-server_peer_tuples = re.findall ("'([\d\.]+)', '([\d]+)'",data)
-print server_peer_tuples
-print len(server_peer_tuples)
-#get remote peers into tuples
-
-#get local peers into tuples
-peer_file = open("peers.txt", 'r')
-peer_tuples = []
-for line in peer_file:
-    extension = re.findall ("'([\d\.]+)', '([\d]+)'",line)
-    peer_tuples.extend(extension)
-peer_file.close()
-print peer_tuples
-#get local peers into tuples
-
-for x in server_peer_tuples:
-    if x not in peer_tuples:
-        print str(x)+" is a new peer, saving."
-
-        peer_list_file = open("peers.txt", 'a')
-        peer_list_file.write(x+"\n")
-        peer_list_file.close()        
-        
-    else:
-        print str(x)+" is not a new peer, skipping."
-
-
-        
-#broadcast
-
-try:
-    conn = sqlite3.connect('ledger.db')
-    c = conn.cursor()
-    c.execute("SELECT block_height FROM transactions ORDER BY block_height DESC LIMIT 1;")
-    block_height = int(c.fetchone()[0])   
-    block_height_new = block_height+1
-
-    #request block update
-    s.sendall (str(block_height))
-    #request block update
-    
-except sqlite3.Error, e:                        
-    print "Error %s:" % e.args[0]
-    sys.exit(1)                        
-finally:                        
-    if conn:
-        conn.close()       
-
-to_address = "dummy2"
-amount = 3
-
-transaction = str(block_height_new) +":"+ str(address) +":"+ str(to_address) +":"+ str(amount)
-signature = key.sign(transaction, '')
-print "Signature: "+str(signature)
-
-if public_key.verify(transaction, signature) == True:
-    print "The signature is valid, proceeding to send transaction, signature and the public key"
-    s.sendall(transaction+";"+str(signature)+";"+public_key_readable)
-
-else:
-    print "Invalid signature"
-
-
-#sync from node
-#request block update
-s.sendall (str(block_height))
-#request block update
-    
-block_difference = s.recv(1024)
-print "Receiving "+block_difference+" steps to sync"
-i = 1
-while int(i) <= int(block_difference):
-    sync = s.recv(1024)
-    i = i+1
-    #verify
-    sync_list = ast.literal_eval(sync) #this is great, need to add it to client -> node sync
-    received_block_height = sync_list[0]
-    received_address = sync_list[1]
-    received_to_address = sync_list[2]
-    received_amount = sync_list [3]
-    received_signature = sync_list[4]
-    received_public_key_readable = sync_list[5]
-    received_public_key = RSA.importKey(sync_list[5])
-    received_transaction = str(received_block_height) +":"+ str(received_address) +":"+ str(received_to_address) +":"+ str(received_amount) #todo: why not have bare list instead of converting?
-    received_signature_tuple = ast.literal_eval(received_signature) #converting to tuple
-    if received_public_key.verify(received_transaction, received_signature_tuple) == True:
-        print "Received step "+str(received_block_height)+" is valid"
-        #verify
-        #save step to db
-        
-        try:
-            conn = sqlite3.connect('ledger.db') #use a different db here for TEST PURPOSES
+            conn = sqlite3.connect('ledger.db')
             c = conn.cursor()
-            c.execute("INSERT INTO transactions VALUES ('"+str(received_block_height)+"','"+str(received_address)+"','"+str(received_to_address)+"','"+str(received_to_address)+"','"+str(received_signature)+"','"+str(received_public_key_readable)+"')") # Insert a row of data
-            print "Ledger updated with a received transaction"
-            conn.commit() # Save (commit) the changes
+            c.execute("SELECT block_height FROM transactions ORDER BY block_height DESC LIMIT 1;")
+            block_height = int(c.fetchone()[0])   
+            block_height_new = block_height+1
+
+            #request block update
+            s.sendall (str(block_height))
+            #request block update
             
         except sqlite3.Error, e:                        
             print "Error %s:" % e.args[0]
             sys.exit(1)                        
         finally:                        
             if conn:
-                conn.close()
-        #save step to db
-        print "Ledger synchronization finished"
-    
-#sync from node
-    
-#broadcast
-s.close()
+                conn.close()       
 
-#network client program
+        to_address = "dummy2"
+        amount = 3
+
+        transaction = str(block_height_new) +":"+ str(address) +":"+ str(to_address) +":"+ str(amount)
+        signature = key.sign(transaction, '')
+        print "Signature: "+str(signature)
+
+        if public_key.verify(transaction, signature) == True:
+            print "The signature is valid, proceeding to send transaction, signature and the public key"
+            s.sendall(transaction+";"+str(signature)+";"+public_key_readable)
+
+        else:
+            print "Invalid signature"
+
+
+        #sync from node
+        #request block update
+        s.sendall (str(block_height))
+        #request block update
+            
+        block_difference = s.recv(1024)
+        print "Receiving "+block_difference+" steps to sync"
+        i = 1
+        while int(i) <= int(block_difference):
+            sync = s.recv(1024)
+            i = i+1
+            #verify
+            sync_list = ast.literal_eval(sync) #this is great, need to add it to client -> node sync
+            received_block_height = sync_list[0]
+            received_address = sync_list[1]
+            received_to_address = sync_list[2]
+            received_amount = sync_list [3]
+            received_signature = sync_list[4]
+            received_public_key_readable = sync_list[5]
+            received_public_key = RSA.importKey(sync_list[5])
+            received_transaction = str(received_block_height) +":"+ str(received_address) +":"+ str(received_to_address) +":"+ str(received_amount) #todo: why not have bare list instead of converting?
+            received_signature_tuple = ast.literal_eval(received_signature) #converting to tuple
+            if received_public_key.verify(received_transaction, received_signature_tuple) == True:
+                print "Received step "+str(received_block_height)+" is valid"
+                #verify
+                #save step to db
+                
+                try:
+                    conn = sqlite3.connect('ledger.db') #use a different db here for TEST PURPOSES
+                    c = conn.cursor()
+                    c.execute("INSERT INTO transactions VALUES ('"+str(received_block_height)+"','"+str(received_address)+"','"+str(received_to_address)+"','"+str(received_to_address)+"','"+str(received_signature)+"','"+str(received_public_key_readable)+"')") # Insert a row of data
+                    print "Ledger updated with a received transaction"
+                    conn.commit() # Save (commit) the changes
+                    
+                except sqlite3.Error, e:                        
+                    print "Error %s:" % e.args[0]
+                    sys.exit(1)                        
+                finally:                        
+                    if conn:
+                        conn.close()
+                #save step to db
+                print "Ledger synchronization finished"
+            
+        #sync from node
+            
+        #broadcast
+        s.close()
+
+        #network client program
+
+
+    except Exception as e:
+        print e
+        print "Cannot connect to "+str(HOST)+" "+str(PORT)
+        pass
 
