0a60b26ce2e5a46c4cb02dc839ba571f0c8333b2
hclivess <translace@gmail.com>
2016-04-08 14:17:43 +0200

    fixes to peer handling

--- compile.cmd
+++ compile.cmd
@@ -2,4 +2,8 @@ pyinstaller.exe --onefile node.py
 pyinstaller.exe --onefile genesis.py
 pyinstaller.exe --onefile gui.py
 pyinstaller.exe --onefile send.py
+del genesis.spec
+del gui.spec
+del node.spec
+del send.spec
 pause
