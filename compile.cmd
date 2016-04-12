6705ed21721ac27b6d0bdd510dd015df9e1beecf
Script Kid <translace@gmail.com>
2016-04-12 21:09:11 +0200

    playing around with graphics

--- compile.cmd
+++ compile.cmd
@@ -1,7 +1,8 @@
-pyinstaller.exe --onefile --noconsole gui.py
-pyinstaller.exe --onefile node.py
-pyinstaller.exe --onefile genesis.py
-pyinstaller.exe --onefile send.py
+pyinstaller.exe --onefile --noconsole gui.py --icon=icon.ico
+pyinstaller.exe --onefile node.py --icon=icon.ico
+pyinstaller.exe --onefile genesis.py --icon=icon.ico
+pyinstaller.exe --onefile send.py --icon=icon.ico
+copy ledger.db dist\ledger.db
 del genesis.spec
 del gui.spec
 del node.spec
