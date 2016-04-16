b6a35abc7ec4ea9395657c8b912ce9f06dd3f419
Script Kid <translace@gmail.com>
2016-04-17 01:17:43 +0200

    awesome tx table in gui

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-from time import strftime
 from datetime import datetime
 
 import hashlib
@@ -9,10 +8,13 @@ import base64
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA
+#from PIL import ImageTk, Image
 
 from Tkinter import *
-window = Tk()
-window.wm_title("[XBM] Bismuth")
+root = Tk()
+
+root.wm_title("[XBM] Bismuth")
+
 
 # import keys
 key = RSA.importKey(open('privkey.der').read())
@@ -30,6 +32,39 @@ except:
     sys.exit(1)
 print "Connected"
 
+def table():
+    # transaction table
+    # data
+    datasheet = ["time","from","to","amount"]
+
+    conn = sqlite3.connect('ledger.db')
+    c = conn.cursor()
+    for row in c.execute("SELECT * FROM transactions WHERE address = '" + str(address) + "' OR to_address = '" + str(
+            address) + "' ORDER BY block_height DESC LIMIT 19;"):
+        db_timestamp = row[1]
+        datasheet.append(datetime.fromtimestamp(float(db_timestamp)).strftime('%Y-%m-%d %H:%M:%S'))
+        db_address = row[2]
+        datasheet.append(db_address)
+        db_to_address = row[3]
+        datasheet.append(db_to_address)
+        db_amount = row[4]
+        datasheet.append(db_amount)
+    conn.close()
+    # data
+
+    print datasheet
+
+    k = 0
+    for i in range(20):
+        for j in range(4):
+            e = Entry()
+            e.grid(row=i + 8, column=j, sticky=NSEW)
+            e.insert(END,datasheet[k])
+
+            k = k + 1
+
+    # transaction table
+
 def balance_get():
     conn = sqlite3.connect('ledger.db')
     c = conn.cursor()
@@ -45,8 +80,15 @@ def balance_get():
     print "Node: Transction address balance: "+str(balance)                       
     conn.close()
     #updata balance label
-    balance_msg = Label(window, text = "Balance: "+str(balance))
-    balance_msg.grid(row = 3, column = 1, columnspan = 3, pady=4)
+    balance_msg = Label(root, text = "Balance: "+str(balance))
+    balance_msg.grid(row = 3, column = 1, sticky=E)
+
+    spent_msg = Label(root, text="Spent Total: " + str(debit))
+    spent_msg.grid(row=4, column=1, sticky=E)
+
+    received_msg = Label(root, text="Received Total: " + str(credit))
+    received_msg.grid(row=5, column=1, sticky=E)
+    table()
 
 def send():
     print "Received tx command"
@@ -90,6 +132,7 @@ def send():
             time.sleep(0.1)
             s.sendall(transaction+";"+str(signature_enc)+";"+public_key_readable+";"+str(txhash_new)) #todo send list
             time.sleep(0.1)
+            balance_get()
         
     else:
         print "Client: Invalid signature"
@@ -102,78 +145,47 @@ def node():
 
 def app_quit():
     print "Received quit command"
-    window.destroy()
+    root.destroy()
 
 balance_get() #get balance on start
+table()
 
 #address and amount
-Label(window, text="To address", width=20).grid(row=0)
-Label(window, text="Amount", width=20).grid(row=1)
+Label(root, text="To address", width=20).grid(row=0)
+Label(root, text="Amount", width=20).grid(row=1)
 
-to_address = Entry(window, width=30)
+to_address = Entry(root, width=57)
 to_address.grid(row=0, column=1, pady=4)
 
-amount = Entry(window, width=30)
+amount = Entry(root, width=57)
 amount.grid(row=1, column=1, pady=4)
 
-balance_enumerator = Entry(window, width=10)
+balance_enumerator = Entry(root, width=10)
 #address and amount
 
 #buttons
 
-send_b = Button(window, text="Send transaction", command=send, height=1, width=15)
-send_b.grid(row=4, column=1, sticky=W, pady=4)
+send_b = Button(root, text="Send transaction", command=send, height=1, width=15)
+send_b.grid(row=3, column=0, sticky=W, pady=4,padx=4)
 
-start_b = Button(window, text="Start node", command=node, height=1, width=15)
-start_b.grid(row=5, column=1, sticky=W, pady=4)
+start_b = Button(root, text="Start node", command=node, height=1, width=15)
+start_b.grid(row=4, column=0, sticky=W, pady=4,padx=4,columnspan=4)
 
-balance_b = Button(window, text="Check balance", command=balance_get, height=1, width=15)
-balance_b.grid(row=6, column=1, sticky=W, pady=4)
+balance_b = Button(root, text="Check balance", command=balance_get, height=1, width=15)
+balance_b.grid(row=3, column=1, sticky=W, pady=4,padx=4)
 
-quit_b = Button(window, text="Quit", command=app_quit, height=1, width=15)
-quit_b.grid(row=7, column=1, sticky=W, pady=4)
+balance_b = Button(root, text="Refresh table", command=table, height=1, width=15)
+balance_b.grid(row=4, column=1, sticky=W, pady=4,padx=4)
+
+quit_b = Button(root, text="Quit", command=app_quit, height=1, width=15)
+quit_b.grid(row=5, column=1, sticky=W, pady=4,padx=4)
 
 #buttons
 
-#transaction table
-#data
-datasheet = []
-
-conn = sqlite3.connect('ledger.db')
-c = conn.cursor()
-for row in c.execute("SELECT * FROM transactions WHERE address = '"+str(address)+"' ORDER BY block_height DESC LIMIT 5;"):
-    db_timestamp = row[1]
-    datasheet.append(datetime.fromtimestamp(float(db_timestamp)).strftime('%Y-%m-%d %H:%M:%S'))
-    db_address = row[2]
-    datasheet.append(db_address)
-    db_to_address = row[3]
-    datasheet.append(db_to_address)
-    db_amount = row[4]
-    datasheet.append(db_amount)
-conn.close()
-#data
-
-print datasheet
-
-rows = []
-for i in range(10):
-    
-    cols = []
-    for j in range(4):
-        e = Entry(relief=RIDGE)
-        e.grid(row=i+8, column=j, sticky=NSEW)
-        e.insert(END,(datasheet[j]))
-        cols.append(e)
-    rows.append(cols)
-
-def onPress():
-    for row in rows:
-        for col in row:
-            print col.get(),
-        print
-
-fetch = Button(text='Fetch', command=onPress)
-fetch.grid()
-#transaction table
+#logo
+logo=PhotoImage(file="graphics/logo.gif")
+image = Label(image=logo)
+image.grid(row=0, column=2, columnspan=2, rowspan=2, sticky=W+E+N+S, padx=5, pady=5)
+#logo
 
 mainloop()
--- node.py
+++ node.py
@@ -664,28 +664,7 @@ def worker(HOST,PORT):
                 
                 #communication starter
 
-                    #purge nodes start
-                    s_purge = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    try:
-                        peer_list = []
 
-                        print "Checking peers"
-                        s_purge.connect((HOST, PORT))#save a new peer file with only active nodes
-                        s_purge.close()
-
-                        peer_list.append("('"+str(HOST)+"', '"+str(PORT)+"')")
-
-                        open("peers.txt", 'w').close() #purge file completely
-                        peer_list_file = open("peers.txt", 'a')
-                        for x in peer_list:
-                            peer_list_file.write(x+"\n")
-                            print x+" kept" #append peers to which connection is possible
-                        peer_list_file.close()
-                    except:
-                        print "Could not connect to "+str(HOST)+":"+str(PORT)+", purged"
-                        #raise #for testing purposes only
-                        break
-                    #purge nodes end
 
                 data = s.recv(11) #receive data, one and the only root point
                 print 'Client: Received data from '+ str(peer) +"\n"+ str(data)   
