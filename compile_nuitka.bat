f5e6911d98e72de082995fbe34f643432f23b982
hclivess <translace@gmail.com>
2016-04-18 16:17:38 +0200

    nuitka testing

--- compile_nuitka.bat
+++ compile_nuitka.bat
@@ -1,3 +1,4 @@
-nuitka --recurse-all gui.py
-nuitka --recurse-all node.py
+#run from vs command line
+nuitka --recurse-all node.py --windows-icon="graphics\icon.ico"
+nuitka --recurse-all gui.py --windows-icon="graphics\icon.ico" --windows-disable-console
 pause
