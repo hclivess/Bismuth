76032cdaadabe01680bbaa0ae0fab64df9994b09
Script Kid <translace@gmail.com>
2016-04-20 00:45:07 +0200

    update

--- compile_pyinstaller.cmd
+++ compile_pyinstaller.cmd
@@ -1,7 +1,7 @@
 del /f /s /q dist 1>nul
 rmdir /s /q dist
-pyinstaller.exe --log-level=INFO gui.py --icon=graphics\icon.ico
-pyinstaller.exe --log-level=INFO node.py --icon=graphics\icon.ico
+pyinstaller.exe --onefile --log-level=INFO gui.py --icon=graphics\icon.ico
+pyinstaller.exe --onefile --log-level=INFO node.py --icon=graphics\icon.ico
 copy peers.txt dist\peers.txt
 copy ledger.db dist\ledger.db
 robocopy /e graphics dist\graphics\
