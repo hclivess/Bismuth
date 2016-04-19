3fbe765fb44ee58b12af312e5ef3ac20a545e0cb
Script Kid <translace@gmail.com>
2016-04-19 07:23:41 +0200

    update

--- compile_pyinstaller.cmd
+++ compile_pyinstaller.cmd
@@ -2,5 +2,5 @@ pyinstaller.exe --hidden-import=tcl --hidden-import=tk --onefile --noconsole gui
 pyinstaller.exe --onefile node.py --icon=graphics\icon.ico
 copy peers.txt dist\
 copy ledger.db dist\
-xcopy graphics dist\graphics\
+xcopy graphics dist\graphics\ /a
 pause
