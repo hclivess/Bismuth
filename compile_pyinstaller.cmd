f99ca864bcae456a95ce5b25bdd1ed568f6ed2e1
hclivess <translace@gmail.com>
2016-04-29 09:04:44 +0200

    graphics embedded for real now

--- compile_pyinstaller.cmd
+++ compile_pyinstaller.cmd
@@ -4,9 +4,6 @@ pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO gui.py --icon
 pyinstaller.exe --uac-admin --onefile --log-level=INFO node.py --icon=graphics\icon.ico
 copy peers.txt dist\peers.txt
 copy ledger.db dist\ledger.db
-robocopy /e graphics dist\graphics\
-del /f /s /q dist\gui 1>nul
-rmdir /s /q dist\gui
 "C:\Program Files (x86)\Inno Setup 5\iscc" /q "setup.iss"
 pause
 
