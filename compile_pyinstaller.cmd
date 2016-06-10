cf0ab94b86619d99b3c8766f0a0a39c6d115ab9c
William Anderson <translace@gmail.com>
2016-06-10 15:40:02 +0200

    explorer now compiles too

--- compile_pyinstaller.cmd
+++ compile_pyinstaller.cmd
@@ -3,6 +3,7 @@ rmdir /s /q dist
 pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO gui.py --icon=graphics\icon.ico
 pyinstaller.exe --uac-admin --onefile --log-level=INFO node.py --icon=graphics\icon.ico
 pyinstaller.exe --uac-admin --onefile --log-level=INFO miner.py --icon=graphics\icon.ico
+pyinstaller.exe --uac-admin --onefile --log-level=INFO explorer\explorer.py --icon=graphics\icon.ico
 copy peers.txt dist\peers.txt
 copy ledger.db dist\ledger.db
 "C:\Program Files (x86)\Inno Setup 5\iscc" /q "setup.iss"
