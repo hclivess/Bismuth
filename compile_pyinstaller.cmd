ed63e0df3b77a80f69118fbdd3d35ef92e10ad0f
Script Kid <translace@gmail.com>
2016-07-27 22:56:34 +0200

    changes following LamaGanja's merge

--- compile_pyinstaller.cmd
+++ compile_pyinstaller.cmd
@@ -3,8 +3,8 @@ rmdir /s /q dist
 pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO gui.py --icon=graphics\icon.ico
 pyinstaller.exe --uac-admin --onefile --log-level=INFO node.py --icon=graphics\icon.ico
 pyinstaller.exe --uac-admin --onefile --log-level=INFO miner.py --icon=graphics\icon.ico
-pyinstaller.exe --uac-admin --onefile --log-level=INFO explorer\explorer.py --icon=graphics\icon.ico --hidden-import=explorer
-robocopy explorer\static dist\static
+pyinstaller.exe --uac-admin --onefile --log-level=INFO explorer.py --icon=graphics\icon.ico --hidden-import=explorer
+robocopy static dist\static
 copy peers.txt dist\peers.txt
 copy ledger.db dist\ledger.db
 "C:\Program Files (x86)\Inno Setup 5\iscc" /q "setup.iss"
