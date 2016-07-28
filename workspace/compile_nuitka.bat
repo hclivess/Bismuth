a47b1076e6f2790eb71bee5b9c5f8196e0704c38
Script Kid <translace@gmail.com>
2016-07-29 01:12:27 +0200

    0.86

--- workspace/compile_nuitka.bat
+++
@@ -1,2 +0,0 @@
-start nuitka --recurse-all --portable --standalone --windows-disable-console --icon="C:\Users\HCLivess\Documents\GitHub\XBM-Bismuth\graphics\icon.ico" gui.py
-start nuitka --recurse-all --portable --standalone --icon="C:\Users\HCLivess\Documents\GitHub\XBM-Bismuth\graphics\icon.ico" node.py
