ee8e1f5af7a0aa6694cb64ab4cccbe27d832971e
hclivess <translace@gmail.com>
2016-07-28 16:08:50 +0200

    plotter added

--- compile_pyinstaller.cmd
+++ compile_pyinstaller.cmd
@@ -4,6 +4,8 @@ pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO gui.py --icon
 pyinstaller.exe --uac-admin --onefile --log-level=INFO node.py --icon=graphics\icon.ico --manifest
 pyinstaller.exe --uac-admin --onefile --log-level=INFO miner.py --icon=graphics\icon.ico --manifest
 pyinstaller.exe --uac-admin --onefile --log-level=INFO explorer.py --icon=graphics\icon.ico --hidden-import=explorer --manifest
+pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO plotter.py --icon=graphics\icon.ico --manifest
+
 robocopy static dist\static
 copy peers.txt dist\peers.txt
 copy ledger.db dist\ledger.db
