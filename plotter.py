b60e33f80c1884b00ceff510abc897dadfe44442
hclivess <translace@gmail.com>
2016-07-28 16:06:23 +0200

    plotter added

--- plotter.py
+++ plotter.py
@@ -7,46 +7,49 @@ c.execute("SELECT * FROM transactions ORDER BY block_height ASC;")
 
 all = c.fetchall()
 
-xaxis0 = []
-yaxis1 = []
-yaxis4 = []
-yaxis8 = []
-yaxis9 = []
+axis0 = []
+axisn = []
+
+axis1 = []
+axis4 = []
+axis8 = []
+axis9 = []
 
 
-
+i = 1
 for x in all:
-    xaxis0.append(x[0]) # append timestamp
+    axis0.append(x[0]) # append timestamp
+    axis1.append(x[1]) # append block height
 
-    yaxis1.append(x[1]) # append block height
-    yaxis4.append(x[4])  # append amount
-    yaxis8.append(x[8])  # append fee
-    yaxis9.append(x[9])  # append reward
+    axis4.append(x[4])  # append amount
+    axis8.append(x[8])  # append fee
+    axis9.append(x[9])  # append reward
+
 
 plt.figure(1)                # the first figure
 
 plt.subplot(221)
 plt.yscale('log')
-plt.plot([xaxis0], [yaxis1], 'ro')
-plt.title('1')
+plt.plot([axis1], [axis0], 'ro')
+plt.title('Blocks per time')
 plt.grid(True)
 
 plt.subplot(222)
 plt.yscale('log')
-plt.plot([xaxis0], [yaxis4], 'ro')
-plt.title('2')
+plt.plot([axis1], [axis4], 'ro')
+plt.title('Amount per block height')
 plt.grid(True)
 
 plt.subplot(223)
 plt.yscale('log')
-plt.plot([xaxis0], [yaxis8], 'ro')
-plt.title('3')
+plt.plot([axis1], [axis8], 'ro')
+plt.title('Fee per block height')
 plt.grid(True)
 
 plt.subplot(224)
 plt.yscale('log')
-plt.plot([xaxis0], [yaxis9], 'ro')
-plt.title('4')
+plt.plot([axis1], [axis9], 'ro')
+plt.title('Reward per block height')
 plt.grid(True)
 
 plt.show()
