1b3aff7dca2e58b9fa6fe6ba3a921ef504588dba
Script Kid <translace@gmail.com>
2016-07-28 23:50:26 +0200

    0.86

--- compile_pyinstaller.cmd
+++ compile_pyinstaller.cmd
@@ -1,10 +1,10 @@
 del /f /s /q dist 1>nul
 rmdir /s /q dist
-pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO gui.py --icon=graphics\icon.ico --manifest
-pyinstaller.exe --uac-admin --onefile --log-level=INFO node.py --icon=graphics\icon.ico --manifest
-pyinstaller.exe --uac-admin --onefile --log-level=INFO miner.py --icon=graphics\icon.ico --manifest
-pyinstaller.exe --uac-admin --onefile --log-level=INFO explorer.py --icon=graphics\icon.ico --hidden-import=explorer --manifest
-pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO plotter.py --icon=graphics\icon.ico --manifest
+pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO gui.py --icon=graphics\icon.ico
+pyinstaller.exe --uac-admin --onefile --log-level=INFO node.py --icon=graphics\icon.ico
+pyinstaller.exe --uac-admin --onefile --log-level=INFO miner.py --icon=graphics\icon.ico
+pyinstaller.exe --uac-admin --onefile --log-level=INFO explorer.py --icon=graphics\icon.ico --hidden-import=explorer
+pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO plotter.py --icon=graphics\icon.ico
 
 robocopy static dist\static
 copy peers.txt dist\peers.txt
