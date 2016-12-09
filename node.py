c0c9e347e9caf29220a3c13957a0f7f3528a41fb
Script Kid <translace@gmail.com>
2016-12-10 00:50:22 +0100

    rollback

--- node.py
+++ node.py
@@ -1144,13 +1144,6 @@ def worker(HOST, PORT):
 
     while True:
         try:
-            r, _, _ = select.select([s], [], [])
-            if r:
-                data = s.recv(11)  # receive data, one and the only root point
-                app_log.info('Client: Received ' + data + ' from ' + this_client)
-            else:
-                app_log.info('Client: Issue with socket select') #connection will be cut in higher except
-                raise
 
             # communication starter
             if first_run == 1:
@@ -1171,6 +1164,14 @@ def worker(HOST, PORT):
                 time.sleep(0.1)
 
             # communication starter
+
+            r, _, _ = select.select([s], [], [])
+            if r:
+                data = s.recv(11)  # receive data, one and the only root point
+                app_log.info('Client: Received ' + data + ' from ' + this_client)
+            else:
+                app_log.info('Client: Issue with socket select') #connection will be cut in higher except
+                raise
 
             consensus_ip = s.getpeername()[0]
             if data == "peers______":
