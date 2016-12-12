26be2af07d4e09a64b9894e68f30e89461adcaf7
hclivess <translace@gmail.com>
2016-12-12 14:15:19 +0100

    wip

--- zircus.py
+++ zircus.py
@@ -14,22 +14,44 @@ public_key_readable = str(key.publickey().exportKey())
 public_key_hashed = base64.b64encode(public_key_readable)
 address = hashlib.sha224(public_key_readable).hexdigest()
 
-winner = [5,6,7,8,9]
-loser = [0,1,2,3,4]
+player = [5,6,7,8,9]
+bank = [0,1,2,3,4]
 
 conn = sqlite3.connect('ledger.db')
 conn.text_factory = str
 c = conn.cursor()
 c.execute("select * from transactions where recipient = '"+address+"'")
-result = c.fetchall()
+result_bets = c.fetchall()
 
-for x in result:
-    print x[7]
-    digit_last = (re.findall("(\d)", x[7]))[-1]
-    print digit_last
-    if int(digit_last) in winner:
-        print "winner"
+c.execute("select * from transactions where address = '"+address+"'")
+result_payouts = c.fetchall()
 
+won_count = 0
+lost_count = 0
+txs_winning = []
+payout_missing = []
 
+for x in result_bets:
+    openfield = x[11]
+    block_hash = x[7]
+    #print block_hash
+    tx_signature = x[5] #unique
+    #print tx_signature
+    digit_last = (re.findall("(\d)", block_hash))[-1]
+    #print digit_last
+    if int(digit_last) in player:
+        #print "player wins"
+        won_count = won_count + 1
+
+        if openfield == base64.b64encode("payout for "+tx_signature):
+            print "paid already"
+        else:
+            payout_missing.append(x)
+    else:
+        #print "bank wins"
+        lost_count = lost_count + 1
+
+print lost_count
+print won_count
 conn.close()
 
