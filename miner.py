17dc79a5bf21823569c70570bc4652ece170dd1a
kucerjan <a>
2016-12-14 10:40:09 +0100

    new mining wip

--- miner.py
+++ miner.py
@@ -108,17 +108,16 @@ def miner(args):
                 # calculate difficulty
                 c.execute("SELECT timestamp FROM transactions WHERE block_height = '" + str(db_block_height) + "'")
                 timestamp_last_block = float(c.fetchall()[-1][0])  # select the reward block
-                # print timestamp_last_block
+                print timestamp_last_block
 
                 c.execute("SELECT timestamp FROM transactions WHERE block_height = '" + str(db_block_height - 1) + "'")
                 timestamp_before_last_block = float(c.fetchall()[-1][0])  # select the reward block
-                # print timestamp_before_last_block
+                print timestamp_before_last_block
 
                 timestamp_difference = timestamp_last_block - timestamp_before_last_block
-                if timestamp_difference < 1:
-                    timestamp_difference = 1
+                print timestamp_difference
 
-                diff = int(math.log(1 / timestamp_difference))
+                diff = int(math.log(10000 / timestamp_difference))
 
                 if diff < 1:
                     diff = 1
