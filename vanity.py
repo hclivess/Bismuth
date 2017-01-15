2b9c2c2f7d0676d75190aab0e07ac9811cfb85dd
Script Kid <translace@gmail.com>
2017-01-15 18:28:27 +0100

    miner fix

--- vanity.py
+++ vanity.py
@@ -7,54 +7,71 @@ from Crypto import Random
 from Crypto.PublicKey import RSA
 #from Crypto.Signature import PKCS1_v1_5
 import hashlib
+from multiprocessing import Process
+from multiprocessing import freeze_support
 
-desired = "hcl"
+desired = ["hclivess", "bismuth", "crypto"]
+threads = 6
 
-log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
-logFile = 'vanity.log'
-my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5 * 1024 * 1024, backupCount=2, encoding=None, delay=0)
-my_handler.setFormatter(log_formatter)
-my_handler.setLevel(logging.INFO)
-app_log = logging.getLogger('root')
-app_log.setLevel(logging.INFO)
-app_log.addHandler(my_handler)
+def search():
+    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+    logFile = 'vanity.log'
+    my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5 * 1024 * 1024, backupCount=2, encoding=None, delay=0)
+    my_handler.setFormatter(log_formatter)
+    my_handler.setLevel(logging.INFO)
+    app_log = logging.getLogger('root')
+    app_log.setLevel(logging.INFO)
+    app_log.addHandler(my_handler)
 
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s %(funcName)s(%(lineno)d) %(message)s')
-ch.setFormatter(formatter)
-app_log.addHandler(ch)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s %(funcName)s(%(lineno)d) %(message)s')
+    ch.setFormatter(formatter)
+    app_log.addHandler(ch)
 
-# key maintenance
-if os.path.isfile("privkey.der") is True:
-    app_log.info("Client: privkey.der found")
-else:
-    address = ""
-    while address.startswith(desired) != True:
-        # generate key pair and an address
-        random_generator = Random.new().read
-        key = RSA.generate(1024, random_generator)
-        public_key = key.publickey()
+    # key maintenance
+    if os.path.isfile("privkey.der") is True:
+        app_log.info("Client: privkey.der found")
+    else:
+        address = ""
+        while not any(x in address for x in desired):
+            # generate key pair and an address
+            random_generator = Random.new().read
+            key = RSA.generate(1024, random_generator)
+            public_key = key.publickey()
 
-        private_key_readable = str(key.exportKey())
-        public_key_readable = str(key.publickey().exportKey())
-        address = hashlib.sha224(public_key_readable).hexdigest()  # hashed public key
+            private_key_readable = str(key.exportKey())
+            public_key_readable = str(key.publickey().exportKey())
+            address = hashlib.sha224(public_key_readable).hexdigest()  # hashed public key
 
-        app_log.info('Generating vanity attempt: '+address)
-        # generate key pair and an address
+            app_log.info('Generating vanity attempt: ' + address)
+            # generate key pair and an address
 
-    app_log.info("Client: Your address: " + str(address))
-    app_log.info("Client: Your private key: " + str(private_key_readable))
-    app_log.info("Client: Your public key: " + str(public_key_readable))
+        app_log.info("Client: Your address: " + str(address))
+        app_log.info("Client: Your private key: " + str(private_key_readable))
+        app_log.info("Client: Your public key: " + str(public_key_readable))
 
-    pem_file = open("privkey.der", 'a')
-    pem_file.write(str(private_key_readable))
-    pem_file.close()
+        pem_file = open("privkey.der", 'a')
+        pem_file.write(str(private_key_readable))
+        pem_file.close()
 
-    pem_file = open("pubkey.der", 'a')
-    pem_file.write(str(public_key_readable))
-    pem_file.close()
+        pem_file = open("pubkey.der", 'a')
+        pem_file.write(str(public_key_readable))
+        pem_file.close()
 
-    address_file = open("address.txt", 'a')
-    address_file.write(str(address) + "\n")
-    address_file.close()
+        address_file = open("address.txt", 'a')
+        address_file.write(str(address) + "\n")
+        address_file.close()
+
+if __name__ == '__main__':
+    freeze_support() #must be this line, dont move ahead
+    instances = range(threads)
+    print instances
+    for q in instances:
+        p = Process(target=search, args=())
+        p.start()
+        print "thread "+str(p)+ " started"
+    for q in instances:
+        p.join()
+        p.terminate()
+
