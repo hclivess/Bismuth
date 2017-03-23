062c30fa06b98e41c433c2e6993ede71e77775fb
Script Kid <translace@gmail.com>
2017-03-23 18:27:39 +0100

    pre-release

--- compile_pyinstaller.cmd
+++ compile_pyinstaller.cmd
@@ -2,11 +2,11 @@ del /f /s /q dist 1>nul
 rmdir /s /q dist
 rmdir /s /q build
 pyinstaller.exe --uac-admin --onefile --noconsole --log-level=INFO gui.py --icon=graphics\icon.ico
-pyinstaller.exe --uac-admin --onefile --log-level=INFO node.py --icon=graphics\icon.ico
 pyinstaller.exe --uac-admin --onefile --log-level=INFO miner.py --icon=graphics\icon.ico
 pyinstaller.exe --uac-admin --onefile --log-level=INFO ledger_explorer.py --icon=graphics\icon.ico --hidden-import=ledger_explorer
 pyinstaller.exe --uac-admin --onefile --log-level=INFO zircodice.py --icon=graphics\icon.ico
 pyinstaller.exe --uac-admin --onefile --log-level=INFO zircodice_web.py --icon=graphics\icon.ico --hidden-import=zircodice_web
+pyinstaller.exe --uac-admin --onefile --log-level=INFO node.py --icon=graphics\icon.ico
 
 robocopy static dist\static
 copy peers.txt dist\peers.txt
