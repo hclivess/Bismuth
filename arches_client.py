1e461dbaaa017d4e55974d474ddb758284f40959
kucerjan <a>
2017-03-27 14:48:48 +0200

    arches client updated

--- arches_client.py
+++ arches_client.py
@@ -1,9 +1,10 @@
+import base64, hashlib, socket, pyping, os, getpass, time, psutil, log, sqlite3
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA
 from simplecrypt import decrypt
 
-import base64, hashlib, socket, pyping, os, getpass, time
+app_log = log.log("arches.log")
 
 # import keys
 if not os.path.exists('privkey_encrypted.der'):
@@ -27,14 +28,24 @@ timestamp = str(time.time())
 
 r = pyping.ping('google.com')                # Need to be root or
 
-openfield = ((socket.gethostname()), r.ret_code, r.destination, r.max_rtt, r.avg_rtt, r.min_rtt, r.destination_ip)
+openfield = "Arches:" + socket.gethostname(), psutil.virtual_memory(), psutil.cpu_times(), r.ret_code, r.destination, r.max_rtt, r.avg_rtt, r.min_rtt, r.destination_ip
 print openfield
 
-transaction = (timestamp, address, address, '%.8f' % 0, 0, openfield)  # this is signed
+transaction = (timestamp, address, address, '%.8f' % 0, 0, str(openfield))  # this is signed
 print transaction
 
 h = SHA.new(str(transaction))
 signer = PKCS1_v1_5.new(key)
 signature = signer.sign(h)
 signature_enc = base64.b64encode(signature)
-app_log.info("Client: Encoded Signature: " + str(signature_enc))
+#app_log.info("Client: Encoded Signature: " + str(signature_enc))
+
+mempool = sqlite3.connect('mempool.db')
+mempool.text_factory = str
+m = mempool.cursor()
+
+m.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?)", (timestamp, address, address, '%.8f' % 0, signature_enc, public_key_hashed, "0", str(openfield)))
+mempool.commit()  # Save (commit) the changes
+mempool.close()
+
+app_log.info("Arches: Mempool updated with a transaction")
