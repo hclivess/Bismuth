6f1cc8e156c3ac663a0172c023175ebce4d0c2e5
William Anderson <translace@gmail.com>
2017-06-03 03:15:41 +0200

    different backup structure

--- static/backup.py
+++ static/backup.py
@@ -3,6 +3,6 @@ import time
 interval = 43200
 
 while True:
-    copyfile("ledger.db", "C:\Users\HCLivess\Google Drive\ledger.db")
+    copyfile("ledger.db", "C:\Users\Meegopad\Google Drive\ledger\ledger.db")
     print("Backup complete at {}, interval of {} minutes ({} hours)".format(time.strftime("%Y/%m/%d,%H:%M:%S", time.gmtime()), interval/60, interval/60/60))
     time.sleep(43200)
