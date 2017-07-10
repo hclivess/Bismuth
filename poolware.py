aa1ba96dad9155946c1941c844629c207175283f
Script Kid <translace@gmail.com>
2017-07-10 21:08:07 +0200

    pool fixes

--- poolware.py
+++ poolware.py
@@ -1,5 +1,6 @@
-import socketserver, connections, time, options, log, sqlite3, ast, socks, hashlib, os, random, re
+import socketserver, connections, time, options, log, sqlite3, ast, socks, hashlib, os, random, re, keys
 (port, genesis_conf, verify_conf, version_conf, thread_limit_conf, rebuild_db_conf, debug_conf, purge_conf, pause_conf, ledger_path_conf, hyperblocks_conf, warning_list_limit_conf, tor_conf, debug_level_conf, allowed, mining_ip_conf, sync_conf, mining_threads_conf, diff_recalc_conf, pool_conf, pool_address, ram_conf) = options.read()
+(key, private_key_readable, public_key_readable, public_key_hashed, address) = keys.read() #import keys
 app_log = log.log("pool.log",debug_level_conf)
 
 def commit(cursor):
