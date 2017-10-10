4ddde94e354e8a18c14e2428614047e1ec69b937
Script Kid <translace@gmail.com>
2017-10-10 17:12:30 +0200

    tokenizer upgrade

--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,6 @@
+#issue token: token:issue:worthless:10000
+#transfer token: token:transfer:worthless:10
+
 import sqlite3
 
 conn = sqlite3.connect('static/ledger.db')
@@ -10,8 +13,11 @@ t = tok.cursor()
 t.execute("CREATE TABLE IF NOT EXISTS transactions (block_height, timestamp, token, address, recipient, amount)")
 tok.commit()
 
-#issue token: token:issue:worthless:10000
-#transfer token: token:transfer:worthless:10:f9975bfc90c90f46768d4165b8201830605402a9ebe759f601677e6d
+t.execute("SELECT block_height FROM transactions ORDER BY block_height DESC LIMIT 1;")
+try:
+    token_last_block = int(t.fetchone())
+except:
+    token_last_block = 0
 
 #print all token issuances
 c.execute("SELECT block_height, timestamp, address, recipient, openfield FROM transactions WHERE openfield LIKE ? ORDER BY block_height ASC;", ("token:issue" + '%',))
@@ -52,50 +58,52 @@ tok.commit()
 for token in tokens_processed:
     print("processing", token)
     c.execute("SELECT block_height, timestamp, address, recipient, openfield FROM transactions WHERE openfield LIKE ? ORDER BY block_height ASC;", ("token:transfer:" + token + ':%' ,))
-    r = c.fetchall()[0]
-    print (r)
+    results = c.fetchall()
+    print (results)
 
-    #address_from = total # init
-    #address_to = 0 # init
+    for r in results:
+        block_height = x[0]
+        print("block_height", block_height)
 
-    block_height = x[0]
-    print("block_height", block_height)
+        timestamp = x[1]
+        print("timestamp", timestamp)
 
-    timestamp = x[1]
-    print("timestamp", timestamp)
+        token = r[4].split(":")[2]
+        print("token", token)
 
-    token = r[4].split(":")[2]
-    print("token", token)
+        sender = r[2]
+        print ("transfer_from", sender)
 
-    sender = r[2]
-    print ("transfer_from", sender)
+        recipient = r[3]
+        print ("transfer_to", recipient)
 
-    recipient = r[3]
-    print ("transfer_to", recipient)
+        transfer_amount = r[4].split(":")[3]
+        print ("transfer_amount",transfer_amount)
 
-    transfer_amount = r[4].split(":")[3]
-    print ("transfer_amount",transfer_amount)
+        #calculate balances
+        t.execute("SELECT sum(amount) FROM transactions WHERE recipient = ? and block_height < ?", (sender,) + (block_height,))
+        try:
+            credit_sender = int(t.fetchone()[0])
+            print("credit_sender", credit_sender)
+        except:
+            credit_sender = 0
 
-    #calculate balances
-    t.execute("SELECT sum(amount) FROM transactions WHERE recipient = ?", (sender,))
-    try:
-        credit_sender = int(t.fetchone()[0])
-    except:
-        credit_sender = 0
+        t.execute("SELECT sum(amount) FROM transactions WHERE address = ? and block_height < ?", (sender,) + (block_height,))
+        try:
+            debit_sender = int(t.fetchone()[0])
+            print ("debit_sender", debit_sender)
+        except:
+            debit_sender = 0
+        #calculate balances
 
-    t.execute("SELECT sum(amount) FROM transactions WHERE address = ?", (sender,))
-    try:
-        debit_sender = int(t.fetchone()[0])
-    except:
-        debit_sender = 0
-    #calculate balances
+        #print all token transfers
+        balance_sender = credit_sender - debit_sender
+        print ("balance_sender", balance_sender)
 
-    #print all token transfers
-
-    if credit_sender - debit_sender > 0:
-        t.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?)", (block_height, timestamp, token, sender, recipient, transfer_amount))
-    else:
-        print ("insufficient funds at", sender)
+        if balance_sender > 0:
+            t.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?)", (block_height, timestamp, token, sender, recipient, transfer_amount))
+        else:
+            print ("invalid transaction by", sender)
 
     tok.commit()
 
