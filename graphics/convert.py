0140cd3d11394e222ba32c1e168f8c46f083964a
Script Kid <translace@gmail.com>
2017-10-28 13:40:34 +0200

    fix travis

--- graphics/convert.py
+++ graphics/convert.py
@@ -1,13 +1,14 @@
-import base64, picklemagic
+import base64, json
 
 with open("logo.gif", "rb") as imageFile:
     str = base64.b64encode(imageFile.read())
     print(len(str))
 
 with open("logo.gif", "rb") as imageFile:
-    str2 = picklemagic.safe_dumps(imageFile.read())
+
+    str2 = json.dumps(list(imageFile.read()))
     print(len(str2))
-    print(picklemagic.safe_loads(str2))
+    print(json.loads(str2))
 
 
 #DEMO FOLLOWS
@@ -21,7 +22,7 @@ frame.grid(row=0, column=1, sticky=W + E + N)
 logo_hash_decoded = base64.b64decode(str)
 logo = PhotoImage(data=logo_hash_decoded)
 
-logo_hash_decoded2 = picklemagic.safe_loads(str2)
+logo_hash_decoded2 = json.loads(str2)
 logo2 = PhotoImage(data=logo_hash_decoded)
 
 image = Label(frame, image=logo).grid(pady=25, padx=50, sticky=N)
