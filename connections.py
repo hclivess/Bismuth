7ba1ac7c2b693f6cc7611e3a4955047e859c58ea
Script Kid <translace@gmail.com>
2017-11-14 13:21:08 +0100

    reorg

--- connections.py
+++ connections.py
@@ -6,6 +6,65 @@ def send(sdef, data, slen):
     sdef.sendall(str(len(str(json.dumps(data)))).encode("utf-8").zfill(slen)+str(json.dumps(data)).encode("utf-8"))
 
 if "Linux" in platform.system():
+    READ_OR_ERROR = select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR | select.POLLNVAL
+    #READ_ONLY = select.POLLIN | select.POLLPRI
+
+    def receive(sdef, slen):
+        try:
+            sdef.setblocking(0)
+            poller = select.poll()
+            poller.register(sdef, READ_OR_ERROR)
+            ready = False
+            while not ready:
+                ready = poller.poll(60000) # timeout is in ms
+            fd, flag = ready[0]
+            if (flag & (select.POLLIN|select.POLLPRI)):
+                data = sdef.recv(slen)
+                if not data:
+                    # POLLIN and POLLHUP are not exclusive. We can have both.
+                    raise RuntimeError("Socket EOF")
+                data = int(data)  # receive length
+            elif (flag & (select.POLLERR | select.POLLHUP | select.POLLNVAL)):
+                raise RuntimeError("Socket error {}".format(flag))
+            else:
+                raise RuntimeError("Socket Unexpected Error")
+            chunks = []
+            bytes_recd = 0
+            while bytes_recd < data:
+                ready = False
+                while not ready:
+                    ready = poller.poll(60000)
+                fd, flag = ready[0]
+                if (flag & (select.POLLIN|select.POLLPRI)):
+                    chunk = sdef.recv(min(data - bytes_recd, 2048))
+                    if not chunk:
+                        raise RuntimeError("Socket EOF")
+                    chunks.append(chunk)
+                    bytes_recd = bytes_recd + len(chunk)
+                elif (flag & (select.POLLERR | select.POLLHUP | select.POLLNVAL)):
+                    raise RuntimeError("Socket Error {}".format(flag))
+                else:
+                    raise RuntimeError("Socket Unexpected Error")
+
+            poller.unregister(sdef)
+            segments = b''.join(chunks).decode("utf-8")
+            return json.loads(segments)
+        except Exception as e:
+            """
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            """
+            # Final cleanup
+            try:
+                poller.unregister(sdef)
+            except Exception as e2:
+                pass
+                #print ("Exception unregistering: {}".format(e2))
+            raise RuntimeError("Connections: {}".format(e))
+
+
+else:
 
     def receive(sdef, slen):
         sdef.setblocking(0)
@@ -38,7 +97,7 @@ if "Linux" in platform.system():
 
         return json.loads(segments)
 
-else:
+if "Linux" in platform.system():
     READ_OR_ERROR = select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR | select.POLLNVAL
     #READ_ONLY = select.POLLIN | select.POLLPRI 
 
