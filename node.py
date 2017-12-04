84fce154c200b3aff1a3999af3ca580f2846ecd7
HCLivess <admin@bismuth.cz>
2017-12-04 08:15:24 -0500

    post-hf cleanup

--- node.py
+++ node.py
@@ -511,97 +511,46 @@ def difficulty(c, mode):
     block_height = int(result[0])
     timestamp_before_last = float(c.fetchone()[1])
 
-    if block_height < 400000: #REMOVE THIS AFTER HF
-        execute_param(c, ("SELECT block_height FROM transactions WHERE CAST(timestamp AS INTEGER) > ? AND reward != 0 ORDER BY block_height ASC"), (timestamp_last - 86400,))  # 86400=24h
-        blocks_list_1440 = c.fetchall()
-        blocks_per_1440 = len(blocks_list_1440)
-        block_time = (1440 / blocks_per_1440) * 60
-        execute(c, ("SELECT difficulty FROM misc ORDER BY block_height DESC LIMIT 1"))
-        diff_block_previous = float(c.fetchone()[0])
-        try:
-            log = float('%.13f' % math.log2(blocks_per_1440 / 1440))
-        except:
-            log = float('%.13f' % math.log2(0.5 / 1440))
-            app_log.info("Difficulty exception triggered! This should not happen!")
-        difficulty = float('%.13f' % (diff_block_previous + log))  # increase/decrease diff by a little
-        # min diff
-        execute_param(c, ("SELECT cast(difficulty as FLOAT) FROM misc WHERE block_height >= ?"), (blocks_list_1440[0][0],))
-        try:
-            diff_blocks_list_1440 = c.fetchall()
-            diff_blocks_list_1440 = [i[0] for i in diff_blocks_list_1440]
-            min_diff = statistics.mean(diff_blocks_list_1440)
-        except Exception:
-            min_diff = 90
-        # print(min_diff)
-        # min diff
-        if difficulty < min_diff:
-            difficulty = float('%.13f' % min_diff)
-            if mode == "verbose":
-                app_log.warning("Difficulty floor reached, difficulty readjusted to {}".format(difficulty))
-        time_now = time.time()
-        if time_now > timestamp_last + 600:  # if 10 minutes passed
-            execute(c, ("SELECT difficulty FROM misc ORDER BY block_height DESC LIMIT 5"))
-            diff_5 = c.fetchall()[0]
-            diff_lowest_5 = float(min(diff_5))
-            if diff_lowest_5 < difficulty:
-                candidate = diff_lowest_5  # if lowest of last 5 is lower than calculated diff
-            else:
-                candidate = difficulty
-            difficulty2 = float('%.13f' % percentage(95, candidate))  # candidate -5%
-        else:
-            difficulty2 = difficulty
-        if difficulty < 90:
-            difficulty = 90
-        if difficulty2 < 90:
-            difficulty2 = 90
-        if mode == "verbose":
-            app_log.warning("Blocks per day: {}".format(blocks_per_1440))
-            app_log.warning("Daily block time: {}".format(block_time))
-            app_log.warning("Difficulty retargeting: {}".format(log))
-            app_log.warning("Difficulty: {} {}".format(difficulty, difficulty2))
-        # return (float(50), float(50)) #TEST ONLY
-        return (float(difficulty), float(difficulty2))
-    else: #KEEP THIS AFTER HF
-        execute_param(c, ("SELECT timestamp FROM transactions WHERE CAST(block_height AS INTEGER) > ? AND reward != 0 ORDER BY timestamp ASC LIMIT 2"), (block_height - 1441,))
-        timestamp_1441 = float(c.fetchone()[0])
-        block_time_prev = (timestamp_before_last - timestamp_1441)/1440
-        timestamp_1440 = float(c.fetchone()[0])
-        block_time = (timestamp_last - timestamp_1440)/1440
-        execute(c, ("SELECT difficulty FROM misc ORDER BY block_height DESC LIMIT 1"))
-        diff_block_previous = float(c.fetchone()[0])
-        # Assume current difficulty D is known
-        D = diff_block_previous
-        # Assume current blocktime is known, calculcated from historic data, for example last 1440 blocks
-        T = block_time
-        # Calculcate network hashrate
-        H = pow(2, D / 2.0) / (T * math.ceil(28 - D / 16.0))
-        # Calculate new difficulty for desired blocktime of 60 seconds
-        Td = 60.00
-        D0 = D
-        Dnew = (2 / math.log(2)) * math.log(H * Td * math.ceil(28 - D0 / 16.0))
-        # Feedback controller
-        Kd = 10
-        Dnew = Dnew - Kd*(block_time - block_time_prev)
-        diff_adjustment = (Dnew - D)/720 #reduce by factor of 720
-        Dnew_adjusted = D + diff_adjustment
-        difficulty = float('%.13f' % (Dnew_adjusted))
-        difficulty2 = difficulty
-        time_now = time.time()
-        if block_time > 70.0:
-            if time_now > timestamp_last + 300:  # if more than 5 minutes passed
-                difficulty2 = difficulty - 1.0
-        if difficulty < 80:
-            difficulty = 80
-        if difficulty2 < 80:
-            difficulty2 = 80
-        if mode == "verbose":
-            app_log.warning("Time to generate block {}: {}".format(block_height,timestamp_last - timestamp_before_last))
-            app_log.warning("Current difficulty: {}".format(D))
-            app_log.warning("Current blocktime: {}".format(T))
-            app_log.warning("Current hashrate: {}".format(H))
-            app_log.warning("New difficulty after adjustment: {}".format(Dnew_adjusted))
-            app_log.warning("Difficulty: {} {}".format(difficulty, difficulty2))
-        return (float(difficulty), float(difficulty2))
+    execute_param(c, ("SELECT timestamp FROM transactions WHERE CAST(block_height AS INTEGER) > ? AND reward != 0 ORDER BY timestamp ASC LIMIT 2"), (block_height - 1441,))
+    timestamp_1441 = float(c.fetchone()[0])
+    block_time_prev = (timestamp_before_last - timestamp_1441)/1440
+    timestamp_1440 = float(c.fetchone()[0])
+    block_time = (timestamp_last - timestamp_1440)/1440
+    execute(c, ("SELECT difficulty FROM misc ORDER BY block_height DESC LIMIT 1"))
+    diff_block_previous = float(c.fetchone()[0])
+    # Assume current difficulty D is known
+    D = diff_block_previous
+    # Assume current blocktime is known, calculcated from historic data, for example last 1440 blocks
+    T = block_time
+    # Calculcate network hashrate
+    H = pow(2, D / 2.0) / (T * math.ceil(28 - D / 16.0))
+    # Calculate new difficulty for desired blocktime of 60 seconds
+    Td = 60.00
+    D0 = D
+    Dnew = (2 / math.log(2)) * math.log(H * Td * math.ceil(28 - D0 / 16.0))
+    # Feedback controller
+    Kd = 10
+    Dnew = Dnew - Kd*(block_time - block_time_prev)
+    diff_adjustment = (Dnew - D)/720 #reduce by factor of 720
+    Dnew_adjusted = D + diff_adjustment
+    difficulty = float('%.13f' % (Dnew_adjusted))
+    difficulty2 = difficulty
+    time_now = time.time()
+    if block_time > 70.0:
+        if time_now > timestamp_last + 300:  # if more than 5 minutes passed
+            difficulty2 = difficulty - 1.0
+    if difficulty < 80:
+        difficulty = 80
+    if difficulty2 < 80:
+        difficulty2 = 80
+    if mode == "verbose":
+        app_log.warning("Time to generate block {}: {}".format(block_height,timestamp_last - timestamp_before_last))
+        app_log.warning("Current difficulty: {}".format(D))
+        app_log.warning("Current blocktime: {}".format(T))
+        app_log.warning("Current hashrate: {}".format(H))
+        app_log.warning("New difficulty after adjustment: {}".format(Dnew_adjusted))
+        app_log.warning("Difficulty: {} {}".format(difficulty, difficulty2))
+    return (float(difficulty), float(difficulty2))
 
 
 global connection_pool
@@ -1207,8 +1156,7 @@ def digest_block(data, sdef, peer_ip, conn, c, mempool, m, hdd, h, hdd2, h2, h3)
                     received_signature_dec = base64.b64decode(received_signature_enc)
                     verifier = PKCS1_v1_5.new(received_public_key)
 
-                    if db_block_height > 400000:
-                        validate_pem(received_public_key_hashed)
+                    validate_pem(received_public_key_hashed)
 
                     hash = SHA.new(str((received_timestamp, received_address, received_recipient, received_amount, received_keep, received_openfield)).encode("utf-8"))
                     if not verifier.verify(hash, received_signature_dec):
@@ -1270,11 +1218,7 @@ def digest_block(data, sdef, peer_ip, conn, c, mempool, m, hdd, h, hdd2, h2, h3)
 
                 mining_hash = bin_convert(hashlib.sha224((miner_address + nonce + db_block_hash).encode("utf-8")).hexdigest())
 
-
-                diff_drop_time = 600
-                if db_block_height > 400000:
-                    diff_drop_time = 300
-
+                diff_drop_time = 300
 
                 mining_condition = bin_convert(db_block_hash)[0:int(diff[0])]
                 if mining_condition in mining_hash:  # simplified comparison, no backwards mining
