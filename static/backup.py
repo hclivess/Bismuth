47f3c70426bf76f72a19cde669d3355992da3279
Aareon Sullivan <askully13@gmail.com>
2018-01-06 23:42:26 -0800

    Update backup.py

--- static/backup.py
+++ static/backup.py
@@ -4,5 +4,8 @@ interval = 43200
 
 while True:
     copyfile("ledger.db", "C:\\Users\\Meegopad\\Google Drive\\ledger\\ledger.db")
-    print("Backup complete at {}, interval of {} minutes ({} hours)".format(time.strftime("%Y/%m/%d,%H:%M:%S", time.gmtime()), interval/60, interval/60/60))
-    time.sleep(43200)
+    print("Backup complete at {}, interval of {} minutes ({} hours)".format(time.strftime("%Y/%m/%d,%H:%M:%S",
+                                                                            time.gmtime()),
+                                                                            interval/60,
+                                                                            interval/60/60))
+    time.sleep(interval)
