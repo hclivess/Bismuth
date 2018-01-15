de4d210e4972efba8eca47b14a443ca3ad64efee
Aareon Sullivan <askully13@gmail.com>
2018-01-15 07:14:59 -0800

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -13,9 +13,9 @@ from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA
 from Crypto import Random
 
-if os.path.isfile("privkey.der") is True:
-    print ("privkey.der found")
-elif os.path.isfile("privkey_encrypted.der") is True:
+if os.path.isfile("privkey.der"):
+    print("privkey.der found")
+elif os.path.isfile("privkey_encrypted.der"):
     print("privkey_encrypted.der found")
 
 else:
@@ -28,21 +28,18 @@ else:
     address = hashlib.sha224(public_key_readable.encode("utf-8")).hexdigest()  # hashed public key
     # generate key pair and an address
 
-    print ("Your address: " + str(address))
-    print ("Your private key:\n " + str(private_key_readable))
-    print ("Your public key:\n " + str(public_key_readable))
+    print("Your address: {}".format(address))
+    print("Your private key:\n {}".format(private_key_readable))
+    print("Your public key:\n {}".format(public_key_readable))
+    
+    with open("privkey.der", "a") as f:
+        f.write(str(private_key_readable))
 
-    pem_file = open("privkey.der", 'a')
-    pem_file.write(str(private_key_readable))
-    pem_file.close()
-
-    pem_file = open("pubkey.der", 'a')
-    pem_file.write(str(public_key_readable))
-    pem_file.close()
-
-    address_file = open("address.txt", 'a')
-    address_file.write(str(address) + "\n")
-    address_file.close()
+    with open("pubkey.der", "a") as f:
+        f.write(str(public_key_readable))
+    
+    with open("address.txt", "a") as f:
+        f.write("{}\n".format(address))
 
 # import keys
 key = RSA.importKey(open('privkey.der').read())
@@ -51,46 +48,48 @@ private_key_readable = str(key.exportKey())
 public_key_readable = str(key.publickey().exportKey())
 address = hashlib.sha224(public_key_readable.encode("utf-8")).hexdigest()
 
-print ("Your address: " + str(address))
-print ("Your private key:\n " + str(private_key_readable))
-print ("Your public key:\n " + str(public_key_readable))
+print("Your address: {}".format(address))
+print("Your private key:\n {}".format(private_key_readable))
+print("Your public key:\n {}".format(public_key_readable))
 public_key_hashed = base64.b64encode(public_key_readable)
 # import keys
 
 timestamp = str(time.time())
-print "Timestamp: " + timestamp
+print("Timestamp: {}".format(timestamp))
 transaction = (timestamp, "genesis", address, str(float(100000000)), "genesis")
 h = SHA.new(str(transaction))
 signer = PKCS1_v1_5.new(key)
 signature = signer.sign(h)
 signature_enc = base64.b64encode(signature)
-print "Encoded Signature: " + str(signature_enc)
+print("Encoded Signature: {}".format(signature_enc))
 block_hash = hashlib.sha224(str((timestamp, transaction)).encode("utf-8")).hexdigest()  # first hash is simplified
-print ("Transaction Hash:" + block_hash)
+print ("Transaction Hash: {}".format(block_hash))
 
-if os.path.isfile("static/ledger.db") is True:
-    print ("You are beyond genesis")
+if os.path.isfile("static/ledger.db"):
+    print("You are beyond genesis")
 else:
     # transaction processing
-    con = None
+    cursor = None
+    mem_cur = None
     try:
         conn = sqlite3.connect('static/ledger.db')
-        c = conn.cursor()
-        c.execute("CREATE TABLE transactions (block_height INTEGER, timestamp, address, recipient, amount, signature, public_key, block_hash, fee, reward, keep, openfield)")
-        c.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", ("1", timestamp, 'genesis', address, '0', str(signature_enc), public_key_hashed, block_hash, 0, 1, 1, 'genesis'))  # Insert a row of data
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE transactions (block_height INTEGER, timestamp, address, recipient, amount, signature, public_key, block_hash, fee, reward, keep, openfield)")
+        cursor.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", ("1", timestamp, 'genesis', address, '0', str(signature_enc), public_key_hashed, block_hash, 0, 1, 1, 'genesis'))  # Insert a row of data
         conn.commit()  # Save (commit) the changes
 
         mempool = sqlite3.connect('mempool.db')
-        m = mempool.cursor()
-        m.execute("CREATE TABLE transactions (timestamp, address, recipient, amount, signature, public_key, keep, openfield)")
+        mem_cur = mempool.cursor()
+        mem_cur.execute("CREATE TABLE transactions (timestamp, address, recipient, amount, signature, public_key, keep, openfield)")
         mempool.commit()
         mempool.close()
 
-        print ("Genesis created, don't forget to change genesis address in the config file")
+        print("Genesis created, don't forget to change genesis address in the config file")
     except sqlite3.Error as e:
-        print ("Error %s:" % e.args[0])
+        print("Error %s:" % e.args[0])
         sys.exit(1)
     finally:
-        if conn:
-            conn.close()
-            # transaction processing
+        if cursor is not None:
+            cursor.close()
+        if mem_cur is not None:
+            mem_cur.close()
