215d211d12b2cfd01119ba88c75a21473926c610
Script Kid <translace@gmail.com>
2018-02-18 19:31:18 +0100

    remove source address from URL

--- bisurl.py
+++ bisurl.py
@@ -7,10 +7,10 @@ def checksum(string):
     return m.hexdigest()[:8]
 
 
-def create_url(app_log, command, address, recipient, amount, openfield):
+def create_url(app_log, command, recipient, amount, openfield):
     if command == "pay":
         openfield_b64_encode = (base64.urlsafe_b64encode(openfield.encode("utf-8"))).decode("utf-8")
-        url_partial = "bis://{}/{}/{}/{}/{}/".format(command,address,recipient,amount,openfield_b64_encode)
+        url_partial = "bis://{}/{}/{}/{}/".format(command,recipient,amount,openfield_b64_encode)
         url_constructed = url_partial+checksum(url_partial)
         app_log.warning(url_constructed)        
         return url_constructed
@@ -18,15 +18,15 @@ def create_url(app_log, command, address, recipient, amount, openfield):
 def read_url(app_log, url):
     url_split = url.split("/")
     app_log.warning(url_split)
-    reconstruct = "bis://{}/{}/{}/{}/{}/".format(url_split[2],url_split[3],url_split[4],url_split[5],url_split[6])
-    openfield_b64_decode = base64.urlsafe_b64decode(url_split[6]).decode("utf-8")
+    reconstruct = "bis://{}/{}/{}/{}/".format(url_split[2],url_split[3],url_split[4],url_split[5],url_split[6])
+    openfield_b64_decode = base64.urlsafe_b64decode(url_split[5]).decode("utf-8")
 
-    if checksum(reconstruct) == url_split[7]:
-        url_deconstructed = url_split[2],url_split[3],url_split[4],url_split[5],openfield_b64_decode
+    if checksum(reconstruct) == url_split[6]:
+        url_deconstructed = url_split[2],url_split[3],url_split[4],openfield_b64_decode
         print ("Checksum match")
         return url_deconstructed
     else:
-        print ("Checksum mismatch",checksum(reconstruct),url_split[7])
+        print ("Checksum mismatch",checksum(reconstruct),url_split[6])
         return
 
 
@@ -35,5 +35,5 @@ if __name__ == "__main__":
     import log
     app_log = log.log("node.log", "WARNING", "yes")
 
-    print ("create_url", create_url (app_log, "pay", "address", "recipient", "10", "eeeeeeeeeasdasdasdasdasdeeeeeeeeeeee"))
-    print ("read_url", read_url(app_log, "bis://pay/address/recipient/10/ZWVlZWVlZWVlYXNkYXNkYXNkYXNkYXNkZWVlZWVlZWVlZWVl/7bfd1630"))
+    print ("create_url", create_url (app_log, "pay", "recipient", "10", "eeeeeeeeeasdasdasdasdasdeeeeeeeeeeee"))
+    print ("read_url", read_url(app_log, "bis://pay/recipient/10/ZWVlZWVlZWVlYXNkYXNkYXNkYXNkYXNkZWVlZWVlZWVlZWVl/8a38ded2"))
