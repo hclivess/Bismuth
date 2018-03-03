9c1dcea63c639ec63ce253e142f978827855e051
Script Kid <translace@gmail.com>
2018-03-03 19:38:27 +0100

    update types

--- zircodice_dappie.py
+++ zircodice_dappie.py
@@ -17,7 +17,7 @@ def fee_calculate(openfield):
         fee = Decimal(fee) + Decimal(10)
     if "alias=" in openfield:
         fee = Decimal(fee) + Decimal(1)
-    return float(fee) #float temporarily
+    return fee
 
 (key, private_key_readable, public_key_readable, public_key_hashed, address) = keys.read()
 
@@ -141,7 +141,7 @@ while True:
             # create transactions for missing payouts
             timestamp = '%.2f' % time.time()
 
-            payout_amount = float(bet_amount * 2) - percentage("5.3", bet_amount)
+            payout_amount = Decimal(bet_amount * 2) - percentage(5.3, bet_amount)
             payout_openfield = "payout for " + tx_signature[:8]
             payout_keep = 0
             fee = fee_calculate(payout_openfield)
