f7f13226a856c52affed9c94e3f9f176cadc6a01
Script Kid <translace@gmail.com>
2018-05-07 07:32:22 +0200

    add logging

--- node.py
+++ node.py
@@ -802,13 +802,13 @@ def blocknf(block_hash_delete, peer_ip, conn, c, hdd, h, hdd2, h2):
 
 
                 for tx in backup_data:
+                    tx_short = "{} - {} to {}: {} ({})".format (tx[1], tx[2], tx[3], tx[4], tx[11])
                     try:
                         if tx[9] == 0:
-                            # commit (mempool)
                             app_log.info(mp.MEMPOOL.merge((tx[1], tx[2], tx[3], tx[4], tx[5], tx[6], tx[10], tx[11]), peer_ip, c, False, revert=True))  # will get stuck if you change it to respect db_lock
-                            tx_short = "{} - {} to {}: {} ({})".format(tx[1], tx[2], tx[3], tx[4], tx[11])
                             app_log.warning("Moved tx back to mempool: {}".format (tx_short))
                     except Exception as e:
+                        app_log.warning ("Failed to move tx back to mempool: {}".format (tx_short))
                         app_log.info (e)
                         pass
 
