3ad351696ad21ac5392031fbecc45ce00af93651
Script Kid <translace@gmail.com>
2018-05-20 21:12:27 +0200

    update balance_nogui

--- balance_nogui.py
+++ balance_nogui.py
@@ -1,54 +1,22 @@
-import sqlite3, keys, options
+import essentials, options, connections, socks
 
 config = options.Get()
 config.read()
-full_ledger = config.full_ledger_conf
-ledger_path = config.ledger_path_conf
-hyper_path = config.hyper_path_conf
 
-(key, private_key_readable, public_key_readable, public_key_hashed, address) = keys.read()
+key, public_key_readable, private_key_readable, encrypted, unlocked, public_key_hashed, address = essentials.keys_load("privkey.der", "pubkey.der")
 
-mempool = sqlite3.connect('mempool.db')
-mempool.text_factory = str
-m = mempool.cursor()
+s = socks.socksocket()
+s.settimeout(10)
+s.connect(("127.0.0.1", 5658))
 
-# include mempool fees
-m.execute("SELECT count(amount), sum(amount) FROM transactions WHERE address = ?;", (address,))
-result = m.fetchall()[0]
-if result[1] != None:
-    debit_mempool = float('%.8f' % (float(result[1]) + float(result[1]) * 0.001 + int(result[0]) * 0.01))
-else:
-    debit_mempool = 0
-# include mempool fees
+connections.send (s, "balanceget", 10)
+connections.send (s, address, 10)
+balanceget_result = connections.receive (s, 10)
+print ("Address balance: {}".format (balanceget_result[0]))
+print ("Address credit: {}".format (balanceget_result[1]))
+print ("Address debit: {}".format (balanceget_result[2]))
+print ("Address fees: {}".format (balanceget_result[3]))
+print ("Address rewards: {}".format (balanceget_result[4]))
+print ("Address balance without mempool: {}".format (balanceget_result[5]))
+# get balance
 
-if full_ledger == 1:
-    conn = sqlite3.connect(ledger_path)
-else:
-    conn = sqlite3.connect(hyper_path)
-
-conn.text_factory = str
-c = conn.cursor()
-c.execute("SELECT sum(amount) FROM transactions WHERE recipient = ?;", (address,))
-credit = c.fetchone()[0]
-c.execute("SELECT sum(amount) FROM transactions WHERE address = ?;", (address,))
-debit = c.fetchone()[0]
-c.execute("SELECT sum(fee) FROM transactions WHERE address = ?;", (address,))
-fees = c.fetchone()[0]
-c.execute("SELECT sum(reward) FROM transactions WHERE address = ?;", (address,))
-rewards = c.fetchone()[0]
-c.execute("SELECT MAX(block_height) FROM transactions")
-bl_height = c.fetchone()[0]
-
-debit = 0 if debit is None else float('%.8f' % debit)
-fees = 0 if fees is None else float('%.8f' % fees)
-rewards = 0 if rewards is None else float('%.8f' % rewards)
-credit = 0 if credit is None else float('%.8f' % credit)
-
-balance = '%.8f' % (credit - debit - fees + rewards - debit_mempool)
-
-print("Public key address: {}".format(address))
-print("Total fees paid: {}".format(fees))
-print("Total rewards mined: {}".format(rewards))
-print("Total tokens received: {}".format(credit))
-print("Total tokens spent: {}".format(debit))
-print("Transction address balance: {}".format(balance))
