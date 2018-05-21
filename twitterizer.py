dd26384d351055e549ebb6d139d1414d029a8092
Script Kid <translace@gmail.com>
2018-05-21 14:40:26 +0200

    update twitterizer

--- twitterizer.py
+++ twitterizer.py
@@ -1,5 +1,6 @@
 import json
 
+tweet_id = "998532029049393152"
 file = open('secret.json','r').read()
 parsed = json.loads(file)
 
@@ -19,11 +20,27 @@ api = tweepy.API(auth)
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print (tweet.text)
-"""
+
 
 search_tweets = api.search("$BIS #Bismuth")
 for tweet in search_tweets:
     print (tweet.text)
+"""
+
+open_status = api.get_status(tweet_id)
+parsed = open_status._json
+favorite_count = parsed ['favorite_count']
+retweet_count = parsed ['retweet_count']
+parsed_text = parsed['text']
 
 
+if "#bismuth" and "$bis" in parsed_text.lower() and retweet_count + favorite_count > 10:
+    qualifies = True
+else:
+    qualifies = False
+
+print (qualifies)
+
+
+#print (json.loads(str(open_status)))
 
