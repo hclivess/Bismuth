63ad6520364ab2ac9e24a7cabd92605e494f148d
Script Kid <translace@gmail.com>
2018-07-09 18:16:03 +0200

    make fuel payouts in integer

--- savings.py
+++ savings.py
@@ -162,7 +162,7 @@ def masternodes_payout(conn,c,index,index_cursor,block_height,timestamp,app_log)
                 app_log.warning ("Masternode payout added: {} {}".format (block_height, address))
 
                 #fuel
-                c.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",(-block_height,timestamp,"masternode",address,"0","0","0",mirror_hash,"0","0","token:transfer","fuel:{}".format(stake)))
+                c.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",(-block_height,timestamp,"masternode",address,"0","0","0",mirror_hash,"0","0","token:transfer","fuel:{}".format(int(float(stake)))))
                 conn.commit()
                 app_log.warning ("Masternode fuel payout added: {} {}".format (block_height, address))
                 #fuel
--- tokensv2.py
+++ tokensv2.py
@@ -188,5 +188,5 @@ def tokens_update(file, ledger, mode, app_log, plugin_manager=None):
 
 if __name__ == "__main__":
     app_log = log.log("tokens.log", "WARNING", True)
-    tokens_update("static/index.db", "static/ledger.db", "normal", app_log)
+    tokens_update("static/index_test.db", "static/test.db", "normal", app_log)
     # tokens_update("tokens.db","reindex")
