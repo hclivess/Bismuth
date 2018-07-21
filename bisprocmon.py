b0f2c00a8aa0655daa048eb48e24c535bc6792bf
Aareon Sullivan <askully13@gmail.com>
2018-07-21 15:20:23 -0700

    Clean up
    Removed tons of the conditionals in lieu of a loop over possible processes and a function to write statuses.

--- bisprocmon.py
+++ bisprocmon.py
@@ -4,152 +4,46 @@ import time
 import tornado.ioloop
 import tornado.web
 
-#print(psutil.pids()) # Print all pids
+_processes_ = [
+    ["Node", "node"],
+    ["Casino Interface", "zircodice_web.py"],
+    ["Casino Engine", "zircodice_dappie.py"],
+    ["Ledger Explorer", "ledger_explorer.py"],
+    ["Wallet", "wallet.py"],
+    ["Anonymizer", "anon_dappie.py"],
+    ["HTML Interpreter", "html_dappie.py"],
+    ["Geo", "geo.py"],
+    ["Bisdom", "bisdom.py"],
+    ["Twitter Bot", "twitterizer.py"],
+    ["Wallet Server", "wallet_server.py"]]
 
 
 class MainHandler(tornado.web.RequestHandler):
+    def write_status(self, name, stat):
+        color = 'green' if stat else 'red'
+        stat = 'OK' if stat else 'Not running'
+        self.write("<font color='{}'>"\
+                   "{}: <strong>{}</strong>"\
+                   "</font>"\
+                   "<br>".format(color, stat, name))
+            
     def get(self):
-
         pnames = []
         del pnames [:]
         for process in psutil.pids():
             try:
-                # print (process)
                 p = psutil.Process(process)  # The pid of desired process
-
-                # print(p.name()) # If the name is "python.exe" is called by python
-                # print(p.cmdline()) # Is the command line this process has been called with
                 pnames.append(p.cmdline())
-
-
             except:
                 pass
-
-        if "node.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Node: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Node: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "zircodice_web.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Casino Interface: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Casino Interface: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "zircodice_dappie.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Casino Engine: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Casino Engine: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "ledger_explorer.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Ledger Explorer: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Ledger Explorer: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "wallet.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Wallet: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Wallet: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "anon_dappie.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Anonymizer: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Anonymizer: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "html_dappie.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("HTML Interpreter: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("HTML Interpreter: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "geo.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Geo: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Geo: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "bisdom.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Bisdom: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Bisdom: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "twitterizer.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Twitter Bot: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Twitter Bot: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        if "wallet_server.py" in str(pnames):
-            self.write("<font color='green'>")
-            self.write("Wallet Server: <strong>OK</strong>")
-            self.write("</font>")
-            self.write("<br>")
-        else:
-            self.write("<font color='red'>")
-            self.write("Wallet Server: <strong>Not running</strong>")
-            self.write("</font>")
-            self.write("<br>")
-
-        self.write("<br>")
-        self.write("Last updated: {}".format(time.strftime("%Y/%m/%d,%H:%M:%S", time.gmtime(time.time()))))
-        #time.sleep(5)
-
+    
+        for process in _processes_:
+            name, fn = process
+            self.write_status(name, fn in str(pnames))
+            
+        last_updated = time.strftime("%Y/%m/%d,%H:%M:%S", time.gmtime(time.time()))
+        self.write("<br>"\
+                   "Last updated: {}".format(last_updated))
 
 
 def make_app():
@@ -158,7 +52,9 @@ def make_app():
         (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
     ])
 
+
 if __name__ == "__main__":
     app = make_app()
     app.listen(80)
+
     tornado.ioloop.IOLoop.current().start()
