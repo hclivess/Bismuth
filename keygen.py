9bdae2ec3e8d8bcddae88fb0aa52ae12203f07b5
Script Kid <translace@gmail.com>
2018-07-23 07:59:19 +0200

    handled by essentials

--- keygen.py
+++ keygen.py
@@ -8,12 +8,3 @@ if __name__ == "__main__":
     essentials.keys_check (app_log)
     key, public_key_readable, private_key_readable, encrypted, unlocked, public_key_hashed, myaddress = essentials.keys_load ("privkey.der", "pubkey.der")
     print("Address:",myaddress)
-    print("Data saved to:","privkey.der", "pubkey.der")
-    f = open("privkey.der", "wt")
-    f.write(private_key_readable)
-    f.close()
-    f = open("pubkey.der", "wt")
-    f.write(public_key_readable)
-    f.close()
- 
-
