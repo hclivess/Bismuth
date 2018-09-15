ff8b084da4eb32b3cf03c9607eb2a2de29782abc
HCLivess <admin@bismuth.cz>
2018-09-15 10:33:57 -0500

    externalize noconf send

--- send_nogui_noconf.py
+++ send_nogui_noconf.py
@@ -58,7 +58,7 @@ c = conn.cursor()
 
 s = socks.socksocket()
 s.settimeout(10)
-s.connect(("127.0.0.1", 5658))
+s.connect(("31.31.75.71", 8150))
 
 connections.send (s, "balanceget", 10)
 connections.send (s, address, 10)  # change address here to view other people's transactions
