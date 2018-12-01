d386f018c644a64564b94cb5428929a1e8fc5dd0
HCLivess <admin@bismuth.cz>
2018-12-01 16:12:23 -0600

    remove old code

--- wallet.py
+++ wallet.py
@@ -1666,7 +1666,6 @@ if __name__ == "__main__":
     app_log = log.log("wallet.log", debug_level, terminal_output)
 
     essentials.keys_check(app_log, "wallet.der")
-    essentials.db_check(app_log)
 
     key, public_key_readable, private_key_readable, encrypted, unlocked, public_key_hashed, myaddress, keyfile = essentials.keys_load(private_key_load, public_key_load)
     print("Keyfile: {}".format(keyfile))
