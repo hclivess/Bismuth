d9f1ca7309da0fa829cd9ddf53e75e3867a12c13
HCLivess <admin@bismuth.cz>
2018-12-01 01:29:09 -0600

    love objects

--- autogame/autogame.py
+++ autogame/autogame.py
@@ -6,9 +6,15 @@ conn.text_factory = str
 c = conn.cursor()
 
 block = 900000
-health = 100
-strength = 10
-alive = True
+
+class Hero:
+    def __init__(self):
+        self.health = 100
+        self.strength = 10
+        self.alive = True
+        self.in_combat = False
+
+hero = Hero()
 
 #trigger is followed by events affected by modifiers
 
@@ -35,7 +41,7 @@ def cycle(block):
     block_hash  = result[0][7]
     return block_hash
 
-while alive:
+while hero.alive:
 
     block_hash = cycle(block)
 
@@ -44,7 +50,7 @@ while alive:
             print("You meet {}".format(triggers[trigger_key]))
 
 
-            while alive:
+            while hero.alive:
                 block_hash = cycle(block)
 
                 for event_key in events:
@@ -52,22 +58,22 @@ while alive:
                         print("Event: {}".format(events[event_key]))
 
                         if events[event_key] == "attack":
-                            print ("{} killed! You stride forward with {} HP...".format(triggers[trigger_key], health))
+                            print ("{} killed! You stride forward with {} HP...".format(triggers[trigger_key], hero.health))
                             break
 
-                        if events[event_key] == "heal":
-                            if health < 100:
-                                health = health + 20
-                                print ("You rest and heal to {} HP...".format(health))
-                            if health > 100:
-                                health = 100
+                        if events[event_key] == "heal" and not hero.in_combat:
+                            if hero.health < 100:
+                                hero.health = hero.health + 20
+                                print ("You rest and heal to {} HP...".format(hero.health))
+                            if hero.health > 100:
+                                hero.health = 100
                             break
 
                         if events[event_key] == "attacked":
-                            health = health - 60
-                            print("{} hit you for 60 HP, you now have {} HP".format(triggers[trigger_key], health))
-                            if health < 1:
-                                alive = False
+                            hero.health = hero.health - 60
+                            print("{} hit you for 60 HP, you now have {} HP".format(triggers[trigger_key], hero.health))
+                            if hero.health < 1:
+                                hero.alive = False
 
                 block = block + 1
                 time.sleep(2)
