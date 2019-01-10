820358aee31ea4763a91fdd1ebab7bb8ce613bd0
HCLivess <admin@bismuth.cz>
2019-01-10 14:44:21 -0600

    add duplication scanner to tar.py

--- static/tar.py
+++ static/tar.py
@@ -7,6 +7,34 @@ from decimal import *
 from quantizer import *
 import process_search
 
+class Tar():
+    def __init__(self):
+        self.hdd = sqlite3.connect("ledger.db", timeout=1)
+        self.hdd.text_factory = str
+        self.h = self.hdd.cursor()
+
+        self.hdd2 = sqlite3.connect("hyper.db", timeout=1)
+        self.hdd2.text_factory = str
+        self.h2 = self.hdd2.cursor()
+        
+        self.errors = 0
+        
+tar_obj = Tar()        
+
+def dupes_check(cursor, name):
+    print (f"Testing {name} for duplicates")
+
+    cursor.execute("SELECT * FROM transactions WHERE signature IN (SELECT signature FROM transactions WHERE signature != '0' GROUP BY signature HAVING COUNT(*) >1)")
+    results = cursor.fetchall()
+
+    dupes_allowed = [708334,708335]
+
+    for result in results:
+        if result[0] not in dupes_allowed:
+            print (f"Duplicate entry on block: {result}")
+            tar_obj.errors += 1
+
+
 
 def balance_from_cursor(cursor, address):
     credit = Decimal("0")
@@ -34,43 +62,35 @@ def balance_from_cursor(cursor, address):
 
     return quantize_eight(credit-debit)
 
-def errors():
-    hdd = sqlite3.connect("ledger.db", timeout=1)
-    hdd.text_factory = str
-    h = hdd.cursor()
-
-    hdd2 = sqlite3.connect("hyper.db", timeout=1)
-    hdd2.text_factory = str
-    h2 = hdd2.cursor()
-
-    ERRORS = 0
+def balance_differences ():
+    
     print ("Selecting all addresses from full ledger for errors")
-    h.execute ("SELECT distinct(recipient) FROM transactions group by recipient;")
-    addresses = h.fetchall ()
+    tar_obj.h.execute ("SELECT distinct(recipient) FROM transactions group by recipient;")
+    addresses = tar_obj.h.fetchall ()
 
     for address in addresses:
         address = address[0]
-        balance1 = balance_from_cursor(h, address)
-        balance2 = balance_from_cursor(h2, address)
+        balance1 = balance_from_cursor(tar_obj.h, address)
+        balance2 = balance_from_cursor(tar_obj.h2, address)
         if (balance1 == balance2):
             check = '  Ok'
         else:
             check = '> Ko'
-            ERRORS += 1
+            tar_obj.errors += 1
 
         if address.lower() != address or len(address) != 56 and (balance1 or balance2) != 0:
-            print (address,'> you dun fukt it up')
+            print (f"{address} > wrong recipient")
 
 
-        print(check, address, balance1, balance2)
+        print(f"{check} {address} {balance1} {balance2}")
 
         if (Decimal(balance1) < 0 or Decimal(balance2) < 0):
             print(address,balance1,balance2)
 
 
-    print("Done, {} Errors.".format(ERRORS))
+    print(f"Done, {tar_obj.errors} errors.")
 
-    if ERRORS > 0:
+    if tar_obj.errors > 0:
         return_value = True
     else:
         return_value = False
@@ -78,9 +98,14 @@ def errors():
     return return_value
 
 
+dupes_check(tar_obj.h, "ledger")
+dupes_check(tar_obj.h2, "hyperblocks")
+balance_differences()
 
-if errors():
-    print("There were errors in the hyperblocks, cannot continue")
+
+if tar_obj.errors > 0:
+    print("There were errors, cannot continue")
+    
 else:
 
     if not process_search.proccess_presence ("node.py"):
